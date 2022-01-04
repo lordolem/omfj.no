e46a810da58ca01d02725db3a45400d5bf462071
Ole Magnus <32321558+lordolem@users.noreply.github.com>
2022-01-04 12:01:32 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/contact.css">
+    <title>OMFJ</title>
+</head>
+<body>
+    <div class="main">
+        <h1>Projects</h1>
+        <?php include "include/nav.php"; ?>
+        <p>Feel fre to contact me if you have any inquiries :)</p>
+        <div class="social-media-links">
+            <a href="https://www.github.com/lordolem">Github</a>
+            <a href="https://www.instagram.com/lordolem">Instagram</a>
+            <a href="https://www.linkedin.com/in/olemagnusjohnsen">Linkedin</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ include/nav.php
@@ -0,0 +1,5 @@
+<ul class="nav">
+    <li><a href="/omfj.no/">HOME</a></li>
+    <li><a href="/omfj.no/projects.php">PROJECTS</a></li>
+    <li><a href="/omfj.no/contact.php">CONTACT</a></li>
+</ul>

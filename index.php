95b568fe5d42d5d448639a949d4f39cc127ed5f5
Ole Magnus <32321558+lordolem@users.noreply.github.com>
2022-01-04 12:02:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/index.css">
+    <title>OMFJ</title>
+</head>
+<body>
+    <div class="main">
+        <h1>Ole Magnus</h1>
+        <?php include "include/nav.php"; ?>
+        <p>Hello, I am a student from Norway studying cybersecurity at the University of Bergen.</p>
+    </div>
+    <div class="footer">
+        <p>
+            <b>BTC: </b> 
+            bc1qupxvngljphj68e80g4mqtqpf90ufyykljczlzz
+        </p>
+
+        <p>
+            <b>XMR: </b>
+            42evhxdZFaxiqrXEz21wtbaQ5qmPkrwceSViSDDbP65BTNYDy8joNqEbsgCsRchXbahiLj9usBcgeBkkYymCo8vEEBVqU3x
+        </p>
+    </div>
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/projects.css">
+    <title>OMFJ</title>
+</head>
+<body>
+    <div class="main">
+        <h1>Projects</h1>
+        <?php include "include/nav.php"; ?>
+        <div class="projects-list">
+            <div>
+                <a href="">Kurvmatte</a>
+                <p>A math game I made for my computer class in high school.</p>
+                <a class="take-a-look" href="">Take a look!</a>
+            </div>
+            <div>
+                <a href="">Miele Logic CLI</a>
+                <p>I commandline-interface for checking the progress of my miele washer/dyer.</p>
+                <a class="take-a-look" href="">Take a look!</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

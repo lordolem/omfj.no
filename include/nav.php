7a87fee1cf1e3acd479163ed502d1871264ffb35
Ole Magnus Johnsen <ole.magnus@me.com>
2022-01-04 16:06:40 +0100

    updated links

--- include/nav.php
+++ include/nav.php
@@ -1,5 +1,5 @@
 <ul class="nav">
     <li><a href="/">HOME</a></li>
-    <li><a href="projects">PROJECTS</a></li>
-    <li><a href="contact">CONTACT</a></li>
+    <li><a href="/projects">PROJECTS</a></li>
+    <li><a href="/contact">CONTACT</a></li>
 </ul>

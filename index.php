5a9fc7fae401dc65d9bcdf4609db864c22e91fc6
Ole Magnus <32321558+lordolem@users.noreply.github.com>
2022-01-09 14:35:45 +0100

    google analytics

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="fooqD9Qy87n5_vHAJpjZy1hhfsrAXujT2cj6z8OVyH4" />
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon.png">
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/index.css">
